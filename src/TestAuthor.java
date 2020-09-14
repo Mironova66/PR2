27f7f5c38b07aa9dd895a44d77ec5cb1c0de1ad2
Katerina <katyusha.agapova@yandex.ru>
2020-09-14 19:31:33 +0300

    Initial commit

---
+++ src/TestAuthor.java
@@ -0,0 +1,10 @@
+public class TestAuthor {
+        public static void main(String[] args)
+        {
+            Author Author1 = new Author("Elena", "elena1990@ya.ru", 'F');
+            Author Author2 = new Author("Ivan", "vanya.efr@ya.ru", 'M');
+            System.out.println(Author1);
+            System.out.println();
+            System.out.println(Author2);
+        }
+}
